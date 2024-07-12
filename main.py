bd3f3b00ffa11cea044347723de2df1fbab1e0ca
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-07-12 18:50:59 +0300

    Add the main method

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import sys
 import logging
-from utils import validate_inputs
+from utils import validate_inputs, generate_combinations, generate_expected_values, is_valid_tc, csv_output
 from error_handling import InvalidInput, InvalidLogic
 
-try:
-    validate_inputs(sys.argv[1:])
-except InvalidLogic as e:
-    print(str(e))
-except InvalidInput as e:
-    print(str(e))
-except Exception as e:
-    logging.exception(e)
-else:
-    print("Valid inputs")
+options = sys.argv[1:]
+while True:
+    try:
+        validate_inputs(options)
+    except (InvalidLogic, InvalidInput) as e:
+        print(str(e))
+        options = input(f"Please provide valid options with space in between\n")
+        options = options.split()
+        continue
+    except Exception as e:
+        logging.exception(e)
+    else:
+        print("Valid inputs")
+        combinations = generate_combinations(options)
+        combinations_expected_values = generate_expected_values(combinations, options)
+        result = is_valid_tc(combinations_expected_values, options)
+        csv_output(result)
+        break
